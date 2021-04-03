188e481960ce829ae38747737689abbb57e72d9c
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2021-04-03 01:15:35 +0100

    -

---
+++ lib/src/main/scala/easy3d/init/Screen.scala
@@ -0,0 +1,16 @@
+package easy3d.init
+
+import org.lwjgl.glfw.GLFW.{glfwGetPrimaryMonitor, glfwGetVideoMode}
+import org.lwjgl.glfw.GLFWVidMode
+
+/**
+ * @author kostas.kougios
+ *         03/04/2021 - 01:11
+ */
+object Screen {
+  def primaryMonitor: Long = glfwGetPrimaryMonitor
+
+  def primaryMonitorVideoMode: GLFWVidMode = videoMode(primaryMonitor)
+
+  def videoMode(monitor: Long) = glfwGetVideoMode(monitor)
+}
--- lib/src/main/scala/g3d/TryApp.scala
+++ lib/src/main/scala/g3d/TryApp.scala
@@ -1,5 +1,6 @@
 package g3d
 
+import easy3d.init.Screen
 import org.lwjgl._
 import org.lwjgl.glfw.Callbacks._
 import org.lwjgl.glfw.GLFW._
@@ -23,19 +24,18 @@ def init(): Unit =
   GLFWErrorCallback.createPrint(System.err).set()
   if (!glfwInit())
     throw new IllegalStateException("Unable to initialize GLFW")
+
+  // Get the resolution of the primary monitor
+  val vidmode = Screen.primaryMonitorVideoMode
+
   // Configure GLFW
   glfwDefaultWindowHints() // optional, the current window hints are already the default
   glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE) // the window will stay hidden after creation
   glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE) // the window will be resizable
 
+
   val window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL)
   if (window == NULL) throw new RuntimeException("Failed to create the GLFW window")
-
-  // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-  glfwSetKeyCallback(window, (window, key, scancode, action, mods) => {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-      glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-  })
 
   val stack = stackPush()
   try {
@@ -43,8 +43,6 @@ def init(): Unit =
     val pHeight = stack.mallocInt(1)
     // Get the window size passed to glfwCreateWindow
     glfwGetWindowSize(window, pWidth, pHeight)
-    // Get the resolution of the primary monitor
-    val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor)
     // Center the window
     glfwSetWindowPos(window, (vidmode.width - pWidth.get(0)) / 2, (vidmode.height - pHeight.get(0)) / 2)
   } finally {
